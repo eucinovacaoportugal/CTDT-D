430f215b02751163ac25a2ca27cd3981679139c1
Manuele Vancin <manuelevancin@Manueles-MacBook-Air.local>
2025-01-23 18:29:16 +0000

    url

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -67,7 +67,7 @@ function App() {
 
   const evaluateTwin = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:5001/evaluate', {
+      const response = await fetch('https://ctdt-d.onrender.com/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
